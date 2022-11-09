5670199d9f8e0a1f08f5be6caf30a39841e60da4
Anastasiia3826 <anastasiyai@rambler.ru>
2022-11-09 22:53:08 +0300

    Add files via upload

---
+++ task11.php
@@ -0,0 +1,37 @@
+<?php
+   $a = 0;
+   $b = 1;
+   $result1 = $a < $b;
+   $result2 = $a == $b;
+   $result3 = $a != $b;
+   $result4 = $a || $b;
+   $result5 = $a && $b;
+   $result6 = $a xor $b;
+   $result6 = $a xor $b;
+
+   echo "</head
+       <body>
+         <table>
+           <thead>
+                <tr>
+                <tr><strong>A</strong></th>
+                <tr><strong>B</strong></th>
+                <tr><strong>!A</strong></th>
+                <tr><strong>A || B</strong></th>
+                <tr><strong>A &amp;&amp; B</strong></th>
+                <tr><strong>A xor B</strong></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>$result1</td>
+                <td>$result2</td>
+                <td>$result3</td>
+                <td>$result4</td>
+                <td>$result5</td>
+                <td>$result6</td>
+                </tr>
+                <tr>
+                <td>0</td>
+                </tbody>
+</table>
